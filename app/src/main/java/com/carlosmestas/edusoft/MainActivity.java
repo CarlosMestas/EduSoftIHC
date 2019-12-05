2ec2602604df7a798dab37f1e0ff2fe791f3beb1
CarlosMestas <cmestas@unsa.edu.pe>
2019-12-04 23:25:12 -0500

    asd

---
+++ app/src/main/java/com/carlosmestas/edusoft/MainActivity.java
@@ -0,0 +1,19 @@
+package com.carlosmestas.edusoft;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
